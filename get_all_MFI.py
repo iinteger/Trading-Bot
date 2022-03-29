c1399a8e7381974f0e655456cd7df29d259e0182
iinteger <42987286+iinteger@users.noreply.github.com>
2022-03-29 19:19:32 +0900

    update code

--- get_all_MFI.py
+++ get_all_MFI.py
@@ -7,6 +7,9 @@ warnings.filterwarnings("ignore")
 
 # BTC ETH ADA
 tickers = pybithumb.get_tickers()
+
+buying_ticker = ["DAI", "NPT"]
+
 for ticker in tqdm(tickers):
     #df = pybithumb.get_ohlcv("XRP")
     df = pybithumb.get_ohlcv(ticker)
@@ -37,36 +40,9 @@ for ticker in tqdm(tickers):
     #df = df.reset_index()
 
     if (df["MFI_14"][-1] <= 0.3 or df["MFI_14"][-2] <= 0.3):
-        print(ticker)
+        print("under MFI 30 :", ticker)
 
-df.drop(["open", "high", "low", "volume", "time"], inplace=True, axis=1)
+    if ((df["MFI_14"][-1] >= 0.7 or df["MFI_14"][-2] >= 0.7) and ticker in buying_ticker):
+        print("over MFI 70 :", ticker)
 
-result = pd.DataFrame()
-for n in [5, 10, 14, 20]:
-    record = []
-    mfi_data = df
-    MFI = mfi_data["MFI_" + str(n)].values
-
-    buy_point_list = (MFI[1:] < 0.3) & (MFI[:-1] >= 0.3)
-    buy_point_list = np.insert(buy_point_list, 0, False)
-    buy_point_list = mfi_data.index[buy_point_list]
-    sell_point_list = (MFI[1:] >= 0.7) & (MFI[:-1] < 0.7)
-    sell_point_list = np.insert(sell_point_list, 0, False)
-    sell_point_list = mfi_data.index[sell_point_list]
-
-    for bp in buy_point_list:
-        if (sum(bp < sell_point_list) > 0) and (bp + 1 <= mfi_data.index[-1]):
-            buy_price = mfi_data.loc[bp + 1, "close"]
-            sp = sell_point_list[sell_point_list > bp][0] + 1
-            if sp <= mfi_data.index[-1]:
-                sell_price = mfi_data.loc[sp, "close"]
-                profit = (sell_price - buy_price) / buy_price * 100
-                record.append(profit)
-        else:
-            break
-
-    result = pd.concat([result, pd.Series(record).describe()], axis = 1)
-
-result.columns = [5, 10, 14, 20]
-print(result)
 # 14일
